6df2f4aa24341341982ab75f3f535bbad815582b
RITVIKTOTAM <74972856+RITVIKTOTAM@users.noreply.github.com>
2021-01-22 17:58:57 +0530

    Add files via upload

---
+++ Plinko.js
@@ -0,0 +1,19 @@
+class Plinko {
+    constructor(x,y,width) {
+      var options = {
+          'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+      }
+      this.body = Bodies.circle(x,y,width,options);
+      this.width = width;
+      World.add(world, this.body);
+    }
+    display(){
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      ellipseMode(CENTER);
+      ellipse( 0, 0, this.width, this.height);
+      pop();
+    }
+  };
