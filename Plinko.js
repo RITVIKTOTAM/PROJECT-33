275b4240a7addd63f641942866083b0b39aaf352
RITVIKTOTAM <74972856+RITVIKTOTAM@users.noreply.github.com>
2021-01-25 20:55:00 +0530

    Update Plinko.js

--- Plinko.js
+++ Plinko.js
@@ -1,6 +1,7 @@
 class Plinko {
     constructor(x,y,width) {
       var options = {
+          'isStatic':false,
           'restitution':0.8,
             'friction':1.0,
             'density':1.0
